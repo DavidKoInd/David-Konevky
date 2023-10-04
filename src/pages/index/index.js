f47dfb91d0ba74f624abfd201f7e738b67cf4622
dakodev <sweetdawinner@gmail.com>
2023-10-03 23:59:36 -0300

    Clean JS Code

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,37 +1,41 @@
-let navImg = document.querySelector(".logo");
-let progressBar = document.querySelector(".progress-bar")
-const b = navImg.getAttribute("class");
-let viewportHeight = window.innerHeight;
-let initialState = true;
-let Pageheight = document.body.scrollHeight;
+const logoElement = document.querySelector(".logo");
+const progressBar = document.querySelector(".progress-bar");
+const colorButtons = document.querySelectorAll(".color");
+const colorNames = ["blue", "orange", "yellow"];
+const viewportHeight = window.innerHeight;
+const pageHeight = document.body.scrollHeight;
 
-function navScroll() {
+function updateProgressBar() {
+    const scrollPercentage = (window.pageYOffset / (pageHeight - viewportHeight)) * 100;
+    progressBar.style.width = `clamp(0px, calc(${scrollPercentage}% + 2.5rem), 100%)`;
+    logoElement.style.left = `clamp(0px, ${scrollPercentage}%, calc(100% - ${logoElement.offsetWidth}px - 0.75rem)`;
+}
+
+function handleScroll() {
     if (window.pageYOffset > 0) {
-        navImg.style.height = `2rem`;
+        logoElement.style.height = `2rem`;
         progressBar.style.height = `0.25rem`;
-        let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let scrolled = (winScroll / height) * 100;
-        progressBar.style.width = `clamp(0px, calc(${scrolled}% + 2.5rem), 100%)`;
-        navImg.style.left = `clamp(0px, ${scrolled}%, calc(100% - ${navImg.offsetWidth}px - 0.75rem)`;
-        return
+        updateProgressBar();
+    } else {
+        progressBar.style.height = `0`;
+        logoElement.style.height = `3rem`;
+        logoElement.style.top = 0;
+        logoElement.style.left = 0;
     }
-    progressBar.style.height = `0`;
-    navImg.style.height = `3rem`;
-    navImg.top = 0;
-    navImg.left = 0;
 }
 
-const colorButton = document.querySelectorAll(".color");
-const colorButtonArray = ["blue", "orange", "yellow"];
-function colorButtonClicker() {
-    for (let i = 0; i < colorButton.length; i++) {
-        colorButton[i].addEventListener('click', () => {
-            document.documentElement.style.setProperty('--background', `var(--${colorButtonArray[i]})`);
+function setColorButtonClickListeners() {
+    colorButtons.forEach((button, i) => {
+        button.addEventListener('click', () => {
+            colorButtons.forEach((btn) => {
+                btn.classList.remove('selected');
+            });
+            button.classList.add('selected');
+            document.documentElement.style.setProperty('--background', `var(--${colorNames[i]})`);
+        });
     });
-    }
+    
 }
-colorButtonClicker()
 
-
-window.onscroll = function () {navScroll()}
+setColorButtonClickListeners();
+window.addEventListener('scroll', handleScroll);
